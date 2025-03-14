e0e8e8a240e016319995933ea505687aa67783a0
T-Terra <gabst138@gmail.com>
2025-03-14 10:05:43 -0300

    feat: responsive table produtcs list

--- frontend/src/components/Table.tsx
+++ frontend/src/components/Table.tsx
@@ -20,24 +20,24 @@ type ListProducts = {
 function TableList({ product, onEdit, onDelete }: ListProducts) {
     return (
       <div className="h-screen w-screen bg-gray-400 flex justify-center">
-        <div className="w-[800px]">
-          <h2 className="text-3xl text-gray-100 font-bold text-center m-3">Listagem de Estoque</h2>
-          <table className="w-[800px] border-gray-400 md:border-separate shadow-2xs">
+        <div className="">
+          <h2 className="text-2xl text-gray-100 sm:text-3xl font-bold text-center m-3">Listagem de Estoque</h2>
+          <table className="w-[400px] sm:w-[800px] border-gray-400 md:border-separate shadow-2xs">
             <thead>
               <tr>
-                <th className="bg-gray-300 text-gray-800 text-left p-3">Nome do Produto</th>
-                <th className="bg-gray-300 text-gray-800 text-left p-3">Código do Produto</th>
-                <th className="bg-gray-300 text-gray-800 text-left p-3">Descrição</th>
-                <th className="bg-gray-300 text-gray-800 text-left p-3">Qt</th>
+                <th className="bg-gray-300 text-gray-800 text-left p-3 text-[11px] sm:text-[15px]">Nome do Produto</th>
+                <th className="bg-gray-300 text-gray-800 text-left p-3 text-[11px] sm:text-[15px]">Código do Produto</th>
+                <th className="bg-gray-300 text-gray-800 text-left p-3 text-[11px] sm:text-[15px]">Descrição</th>
+                <th className="bg-gray-300 text-gray-800 text-left p-3 text-[11px] sm:text-[15px]">Qt</th>
               </tr>
             </thead>
             <tbody>
               {product.map((prod) => (
                   <tr key={prod.id} className="border-b bg-gray-300 text-gray-800 hover:bg-gray-500 hover:text-gray-100">
-                    <td className="p-3">{prod.name}</td>
-                    <td className="p-3">{prod.code}</td>
-                    <td className="p-3">{prod.description}</td>
-                    <td className="p-3">{prod.amount}</td>
+                    <td className="p-3 text-[11px] sm:text-[15px]">{prod.name}</td>
+                    <td className="p-3 text-[11px] sm:text-[15px]">{prod.code}</td>
+                    <td className="p-3 text-[11px] sm:text-[15px]">{prod.description}</td>
+                    <td className="p-3 text-[11px] sm:text-[15px]">{prod.amount}</td>
                     <td>
                       <button
                         onClick={() => onEdit(prod, prod.id)}
